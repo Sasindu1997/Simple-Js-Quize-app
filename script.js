7cd01e27cd58163e05b0535fcd1aee03bb91c07a
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-06-19 00:36:00 +0530

    change button color and body color according to the answer4

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffleQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log('started')
@@ -44,9 +48,33 @@ function resetState(){
      }
 }
 
-function selectAnswer() {
-
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffleQuestions.length > currentQuestionIndex +1) {
+        nextButton.classList.remove('hide')
+    }
+    
 }
+
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if(correct){
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
 const questions = [{
     question: 'What is 2 + 2?',
     answers: [{
